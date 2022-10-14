85c30c54ced1496bc84dac6d6215e7f287d8f525
droidsam1 <droidsam@gmail.com>
2022-10-14 20:13:46 +0200

    refactor: reduce duplicity

--- src/main/java/com/droidsam/app/RomanNumerals.java
+++ src/main/java/com/droidsam/app/RomanNumerals.java
@@ -3,28 +3,18 @@ package com.droidsam.app;
 public class RomanNumerals {
     public static String getRomanRepresentationFor(int arabicNumber) {
         StringBuilder romaRepresentation = new StringBuilder();
-        for(int i = 0; i <arabicNumber; i++){
-            romaRepresentation.append("I");
-        }
 
-        if(arabicNumber == 4){
+        if (arabicNumber == 4) {
             return "IV";
         }
 
-        if(arabicNumber == 5){
-            return "V";
+        if (arabicNumber >= 5) {
+            romaRepresentation.append("V");
+            arabicNumber -= 5;
         }
 
-        if(arabicNumber == 6){
-            return "VI";
-        }
-
-        if(arabicNumber == 7){
-            return "VII";
-        }
-
-        if(arabicNumber == 8){
-            return "VIII";
+        for (int i = 0; i < arabicNumber; i++) {
+            romaRepresentation.append("I");
         }
 
 
