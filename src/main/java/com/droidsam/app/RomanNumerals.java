81a374c803e8ebf6687857ab40dd4a42fa608a12
droidsam1 <droidsam@gmail.com>
2022-10-15 18:56:01 +0200

    refactor: reduce duplicity, remove all conditionals

--- src/main/java/com/droidsam/app/RomanNumerals.java
+++ src/main/java/com/droidsam/app/RomanNumerals.java
@@ -9,6 +9,11 @@ public class RomanNumerals {
 
     private static Map<Integer, String> populateRomanRepresentations() {
         Map<Integer, String> romanRepresentations = new LinkedHashMap<>();
+        romanRepresentations.put(100, "C");
+        romanRepresentations.put(50, "L");
+        romanRepresentations.put(40, "XL");
+        romanRepresentations.put(10, "X");
+        romanRepresentations.put(9, "IX");
         romanRepresentations.put(5, "V");
         romanRepresentations.put(4, "IV");
         romanRepresentations.put(1, "I");
@@ -18,39 +23,12 @@ public class RomanNumerals {
     public static String getRomanRepresentationFor(int arabicNumber) {
         StringBuilder romaRepresentation = new StringBuilder();
 
-        while (arabicNumber >= 100) {
-            romaRepresentation.append("C");
-            arabicNumber -= 100;
-        }
-
-        if (arabicNumber >= 50) {
-            romaRepresentation.append("L");
-            arabicNumber -= 50;
-        }
-
-        if (arabicNumber >= 40) {
-            romaRepresentation.append("XL");
-            arabicNumber -= 40;
-        }
-
-        while (arabicNumber >= 10) {
-            romaRepresentation.append("X");
-            arabicNumber -= 10;
-        }
-
-        if (arabicNumber == 9) {
-            romaRepresentation.append("IX");
-            arabicNumber -= 9;
-        }
-
-
         for (Map.Entry<Integer, String> representations : romanRepresentations.entrySet()) {
             while (arabicNumber >= representations.getKey()) {
                 romaRepresentation.append(representations.getValue());
                 arabicNumber -= representations.getKey();
             }
         }
-
 
         return romaRepresentation.toString();
     }
